182a770efad3884d42d9613c1d0b3d6ef452a7e9
Abubakar <abubakarsiddikl.info@gmail.com>
2023-05-03 00:44:16 +0600

    new Changes

--- main.js
+++ main.js
@@ -31,14 +31,27 @@ const newsByCategories = async (categoryId) => {
     const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`;
     const res = await fetch(url)
     const data = await res.json();
-    document.getElementById('news-section').textContent='';
-    if((data.data).length===0){
+    document.getElementById('news-section').textContent = '';
+    if ((data.data).length === 0) {
         document.getElementById('no-news-messeage').classList.remove('d-none')
     }
-    else{
+    else {
         document.getElementById('no-news-messeage').classList.add('d-none')
     }
-    loadNews(data.data);
+    const showAllBtnParent= document.getElementById('showAllBtn')
+    const showAllBtn= document.createElement('div')
+    showAllBtn.classList="btn btn-danger d-none";
+    showAllBtn.innerHTML=`<div onclick="loadNews(${data.data})">Show All</div>`;
+    showAllBtnParent.appendChild(showAllBtn);
+
+    if((data.data).length>5){
+        loadNews((data.data).slice(0,5));
+        showAllBtn.classList.remove('d-none')
+    }
+    else{
+        loadNews(data.data);
+        showAllBtn.classList.add('d-none')
+    }
 }
 
 const loadNews = (newss) => {
@@ -46,21 +59,24 @@ const loadNews = (newss) => {
     for (const news of newss) {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="d-flex shadow p-3 mb-5 bg-body-tertiary rounded h-100" onclick="newsByCategories(${news._id})">
+        <div class="d-flex shadow p-3 mb-5 bg-body-tertiary rounded h-100" onclick=newsmodal('${news._id}') data-bs-toggle="modal" data-bs-target="#staticBackdrop">
         <img src="${news.image_url}" alt="" class="w-25 me-4 h-100 news-img">
         <div>
             <h4>${news.title}</h4>
-            <p>${(news.details).slice(0,350)}...</p>
+            <p>${(news.details).slice(0, 350)}</p>
             <div class="d-flex justify-content-between">
                 <div class="d-flex align-items-center">
                     <img src="${news.author.img}"" class="user-image me-2">
                     <div>
-                        <p class="mb-0">${news.author.name?? 'No Information'}</p>
+                        <p class="mb-0">${news.author.name ?? 'No Information'}</p>
                         <p class="mb-0">${news.author.published_date}</p>
                     </div>
                 </div>
-                <div class="m-2">
-                    <i class="bi bi-eye"></i>
+                <div class="m-2 d-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                    </svg>
                     <h6>${news.total_view ?? 'No Information'}</h6>
                 </div>
                 <div>
@@ -105,6 +121,41 @@ const loadNews = (newss) => {
     }
 }
 
-const newsmodal= newsData=>{
-    const url = ''
+
+const newsmodal = async (newsData) => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsData}`;
+    const res = await fetch(url)
+    const data = await res.json();
+    loadNewsModal(data.data[0]);
+}
+
+const loadNewsModal = modalinfo => {
+    const modalTitle = document.getElementById('staticBackdropLabel');
+    modalTitle.innerText = modalinfo.title
+    const modalBody = document.getElementById('modalBody');
+    modalBody.textContent='';
+    const modalBodyCreate = document.createElement('div')
+    modalBodyCreate.innerHTML=`
+                <p>${modalinfo.details}</p>
+                <div class="d-flex justify-content-between">
+                  <div class="d-flex align-items-center">
+                    <img src="${modalinfo.author.img}" class=" user-image me-2">
+                    <div>
+                      <p class="mb-0">${modalinfo.author.name ?? 'No Information'}</p>
+                      <p class="mb-0">${modalinfo.author.published_date}</p>
+                    </div>
+                  </div>
+                  <div class="m-2 d-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye"
+                      viewBox="0 0 16 16">
+                      <path
+                        d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                      <path
+                        d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                    <h6>${modalinfo.total_view ?? 'No Information'}</h6>
+                  </div>
+                </div>
+    `
+    modalBody.appendChild(modalBodyCreate);
 }
